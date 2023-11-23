2673316f93b2f576306e59dd5dc810e7b78e2e2d
Felipe <lipesmaffini@gmail.com>
2023-11-23 01:04:33 -0400

    Add project files.

---
+++ Udemy_Dict/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Enter file name(with extension): ");
+string fileName = Console.ReadLine();
+
+
+var info = new DirectoryInfo(Environment.CurrentDirectory);
+
+// Get "documents\\pdf":
+var path = info.Parent.Parent.Parent.Parent.FullName + Path.DirectorySeparatorChar;
+
+string[] line;
+Dictionary<string,int> votes = new Dictionary<string,int>();
+try
+{
+    using (StreamReader sr = File.OpenText(path+fileName))
+    {
+        while (!sr.EndOfStream)
+        {
+            line = sr.ReadLine().Split(';');
+
+            if (votes.ContainsKey(line[0]))
+            {
+                votes[line[0]] += int.Parse(line[1]);
+            }
+            else
+            {
+                votes[line[0]] = int.Parse(line[1]);
+            }            
+        }
+    }
+    foreach(var vote in votes)
+    {
+        Console.WriteLine(vote.Key+": "+vote.Value);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
